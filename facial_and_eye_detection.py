51e91162ab3de167fd3fd8014b29042c88512aab
caoimheharvey <caoimhe.e.harvey@gmail.com>
2017-05-22 17:06:30 +0800

    Comments added

--- facial_and_eye_detection.py
+++ facial_and_eye_detection.py
@@ -10,23 +10,31 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
+#gets main camera for device
 cap = cv2.VideoCapture(0)
 
 while 1:
     ret, img = cap.read()
+    #converting color
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #to find faces
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+    #draw rectangle on detected faces
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 1)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         
+        #detecting an eye within the face
         eyes = eye_cascade.detectMultiScale(roi_gray)
+        #putting a square around the eyes
         for (ex,ey,ew,eh) in eyes:
-            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 1)
 
     cv2.imshow('img',img)
+
+    #pressing the esc key to exit window
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
