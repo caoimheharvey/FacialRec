56503a563b079f4eb291dcb943668cc5c561d1c1
caoimheharvey <caoimhe.e.harvey@gmail.com>
2017-05-31 17:37:01 +0800

    Static Image Face Detection added

---
+++ static-img.py
@@ -0,0 +1,29 @@
+#Caoimhe Harvey
+#File deals with uploading and recognizing face in a picture
+import cv2
+import sys
+
+casc = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+#get image path from command line
+imagePath = sys.argv[1] 
+
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#code to detect the faces
+faces = casc.detectMultiScale (
+	gray,
+	scaleFactor = 1.1,
+	minNeighbors = 5,
+	minSize = (30,30),
+	flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+	)
+
+print "Found {0} faces!".format(len(faces))
+
+for (x,y,w,h) in faces:
+	cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0), 2)
+
+cv2.imshow("Faces Found: ", image)
+cv2.waitKey(0)
