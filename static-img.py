6afe18fb2a6d65d8924f673755e339e4a60a958d
caoimheharvey <caoimhe.e.harvey@gmail.com>
2017-05-31 17:24:58 +0800

    facial rec working, lacking accuracy

--- static-img.py
+++ static-img.py
@@ -1,9 +1,9 @@
 #Caoimhe Harvey
 #File deals with uploading and recognizing face in a picture
 import cv2
-import numpy as np
+import sys
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+casc = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 #get image path from command line
 imagePath = sys.argv[1] 
@@ -12,7 +12,7 @@ image = cv2.imread(imagePath)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 #code to detect the faces
-faces = faceCascade.detectMultiScale (
+faces = casc.detectMultiScale (
 	gray,
 	scaleFactor = 1.1,
 	minNeighbors = 5,
